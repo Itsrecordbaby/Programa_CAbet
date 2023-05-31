af9d80618af8b8b07062c2d8519081b05be56eb0
Mati <amaticc03@gmail.com>
2023-05-31 12:41:09 -0500

    finalfinal

--- Programafinal.c
+++ Programafinal.c
@@ -1,131 +1,180 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <locale.h>
-#include <time.h>
+#include <stdio.h>  //Libreria que nos permite usar las funciones del programa
+#include <stdlib.h> //Libreria que nos permite usar las funciones del programa
+#include <string.h> //Libreria que nos permite usar las funciones del programa
+#include <locale.h> //Libreria que nos permite usar las funciones del programa
+#include <time.h>   //Libreria que nos permite usar las funciones del programa
 
-#define NUM_ANIOS 5
-#define NUM_CARRERAS 7
-#define NUM_GENEROS 2
+// Creacion de la funcion menu y un arreglo unidimensional para las carreras
+int Menu(int flag, int estudiantesmat[5][7][2]);
+char *Carreras[7] = {"Ingenieria de Software", "Matematicas", "Ingenieria Industrial", "Contabilidad", "Administracion", "Relaciones Internacionales", "Economia"};
+// Creacion de la funcion main que contiene el codigo principal
+int main()
+{
 
-const char *Carreras[NUM_CARRERAS] = {"Ingenieria de Software","Matematicas","Ingenieria Industrial","Contabilidad","Administracion","Relaciones Internacionales","Economia"};
+    // Se inicializan los numeros aleatorios
+    srand(time(NULL));
+    // Creacion de un arreglo tridimensional que contiene los 5 años, las 7 carreras y los 2 semestres.
+    int estudiantesmat[5][7][2] = {0};
+    // Creacion de la variable booleana flag para el menu
+    int flag = 1;
 
-void calcularMax(int cantmax[], int size, int *maxIndex, int *maxValue){
-    *maxIndex = 0;
-    for(int i = 1; i < size; i++) {
-        if(cantmax[i] > cantmax[*maxIndex]) {
-            *maxIndex = i;
-        }
-    }
-    *maxValue = cantmax[*maxIndex];
-}
-
-void op1(int alumnosMatri[NUM_ANIOS][NUM_CARRERAS][NUM_GENEROS]){
-    int maxIndex, maxValue;
-    int cantmax[NUM_ANIOS] = {0};
-
-    for (int i = 0; i < NUM_ANIOS; i++) {
-        for (int j = 0; j < NUM_CARRERAS; j++) {
-            for (int k = 0; k < NUM_GENEROS; k++) {
-                cantmax[i] += alumnosMatri[i][j][k];
+    // Uso de triple ciclo For para rellenar la matriz de alumnos matriculados con los datos generados por elcomando rand()
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 7; j++)
+        {
+            for (int k = 0; k < 2; k++)
+            {
+                estudiantesmat[i][j][k] = rand() % 101;
             }
         }
     }
 
-    calcularMax(cantmax, NUM_ANIOS, &maxIndex, &maxValue);
-
-    printf("Respuesta: El anio en que ingreso la mayor cantidad de alumnos fue %d con un total de %d alumnos\n\n", maxIndex+1, maxValue);
-}
-
-void op2(int alumnosMatri[NUM_ANIOS][NUM_CARRERAS][NUM_GENEROS]){
-    int maxIndex, maxValue;
-    int cantmax[NUM_CARRERAS] = {0};
-
-    for (int i = 0; i < NUM_CARRERAS; i++) {
-        for (int j = 0; j < NUM_GENEROS; j++) {
-            cantmax[i] = alumnosMatri[NUM_ANIOS-1][i][j];
-        }
+    // Se pone en pantalla el menu hasta que el usuario decida cerrar el programa mientras la variable flag sea verdadera
+    while (flag)
+    {
+        flag = Menu(flag, estudiantesmat);
     }
 
-    calcularMax(cantmax, NUM_CARRERAS, &maxIndex, &maxValue);
-
-    printf("Respuesta: La Carrera que recibio la mayor cantidad de alumnos en el ultimo anio es %s con un total de %d alumnos\n\n", Carreras[maxIndex], maxValue);
+    return 0;
 }
-
-void op3(int alumnosMatri[NUM_ANIOS][NUM_CARRERAS][NUM_GENEROS]){
-    int maxIndex, maxValue;
-    int cantmax[NUM_ANIOS] = {0};
-
-    for (int i = 0; i < NUM_ANIOS; i++) {
-        for (int j = 0; j < NUM_GENEROS; j++) {
-            cantmax[i] = alumnosMatri[i][0][j];
-        }
-    }
-
-    calcularMax(cantmax, NUM_ANIOS, &maxIndex, &maxValue);
-
-    printf("Respuesta: El anio en que la carrera de Ingenieria de Software recibio la mayor cantidad de alumnos fue el %d con %d alumnos\n\n", maxIndex+1, maxValue);
-}
-
-int menumostrar(int flag, int alumnosMatri[NUM_ANIOS][NUM_CARRERAS][NUM_GENEROS]) {
+// Funcion que permite al usuario ver el Menu
+int Menu(int flag, int estudiantesmat[5][7][2])
+{
+    // Creacion de las variables para que el usuario pueda ingresar la opcion que desee y la variablle que se encarga de verificar si la respuesta colocada por el usuario es correcta
     int opc, result;
+    int Menu = 1; // Variable booleana para observar el menu
 
-    do {
+    // Uso de la variable booleana menumostrar para que cuando sea verdadera, muestre el menu
+    while (Menu)
+    {
         printf("------------------Bienvenido-------------------\n");
-        printf("Seleccione una de las opciones:\n 1. El ano en que ingreso la mayor cantidad de alumnos a la universidad.\n 2.La Carrera que recibio la mayor cantidad de alumnos en el ultimo anio.\n 3.Ano en que la carrera de Ingenieria de Software recibio la mayor cantidad de alumnos\n 4. Salir del programa\n");
+        printf("Ingrese el numero de la opcion que desea:\n 1.El numero de estudiantes de cada anio en su respectiva carrera \n 2.El anio en que ingreso la mayor cantidad de alumnos a la universidad.\n 3.La Carrera que recibio la mayor cantidad de alumnos en el ultimo anio.\n 4.Anio en que la carrera de Ingenieria de Software recibio la mayor cantidad de alumnos\n 5. Salir del programa\n");
         result = scanf("%d", &opc);
+        // En caso de que la entrada ingresada no sea correcta, se limpia el buffer de entrada y se le pide que ingrese de nuevo otro dato
+        while (result != 1)
+        {
+            while (getchar() != '\n')
+                ; // Se encarga de limpiar el buffer de entrada
 
-        while (result != 1) {
-            while (getchar() != '\n');
             printf("\nOpcion incorrecta. Ingrese un numero de nuevo: ");
             result = scanf("%d", &opc);
         }
 
-        switch (opc) {
+        // Dependiendo de la eleccion elegida por el usuario, se ejecuta una serie de comandos gracias al condicional switch case
+        switch (opc)
+        {
         case 1:
-            op1(alumnosMatri);
+            Menu = 0; // Se deja de visualizar el menu
+            // Se imprime las carreras
+            printf("Las carreras son:\n 1. Ingenieria de Software. \n 2. Matematicas\n 3. Ingenieria Industrial\n 4. Contabilidad\n 5. Administracion\n 6. Relaciones Internacionales\n 7. Economia");
+            // Uso de un triple for para recorrer años, carrears y semestres
+            for (int i = 0; i < 5; i++)
+            {
+                printf("\nAnio %d:\n", i + 1);
+                for (int j = 0; j < 7; j++)
+                {
+                    for (int k = 0; k < 2; k++)
+                    {
+                        printf("El numero de alumnos en %s en el semestre %d es: ", Carreras[j], k + 1);
+                        printf("%d\n", estudiantesmat[i][j][k]);
+                        // Se imprime los 5 años con las 7 carreras y sus respectivos dos semestres
+                    }
+                }
+            }
             break;
         case 2:
-            op2(alumnosMatri);
+            Menu = 0;              // Se deja de visualizar el menu
+            int max1;              // Variable a la que se le asignara el numero de los alumnos
+            int cantmax1[5] = {0}; // Creacion de un arreglo unidimensional para los años
+            // Triple ciclo For para crear una suma de todos los alumnos que fueron matriculados cada anio
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    for (int k = 0; k < 2; k++)
+                    {
+                        cantmax1[i] += estudiantesmat[i][j][k];
+                    }
+                }
+            }
+            // Uso de un ciclo For y un condicional If para encontrar el anio con una mayor cantidad de alumnos en el segundo semestre
+            max1 = 0;
+            for (int i = 1; i < 5; i++)
+            {
+                if (cantmax1[i] > cantmax1[max1])
+                {
+                    max1 = i;
+                }
+            }
+            // Imprime el resultado
+            printf("Respuesta: El anio en que ingreso la mayor cantidad de alumnos fue el %d con un total de %d alumnos\n\n", max1 + 1, cantmax1[max1]);
             break;
         case 3:
-            op3(alumnosMatri);
+            Menu = 0;        // Se deja de visualizar el menu
+            int max2 = 0;        // Variable a la que se le asignara el numero de los alumnos
+            int cantmax2[7] = {0}; // Creacion de un arreglo unidimensional para las carreras
+
+            // Doble ciclo For para colocar a la cantidad maxima en las carreras con lo que tenemos gracias al arreglo estudiantesmat
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    cantmax2[i] = cantmax2[i] + estudiantesmat[4][i][j];
+                }
+            }
+
+            // Encuentra la carrera con mas alumnos gracias a un ciclo for y un condicional if que ira ordenando los numeros
+            for (int i = 0; i < 7; i++)
+            {
+                if (cantmax2[i] > cantmax2[max2])
+                {
+                    max2 = i;
+                }
+            }
+            // Imprime la respuesta
+            printf("Respuesta: La Carrera que recibio la mayor cantidad de alumnos en el segundo semestre en el ultimo anio es %s con un total de %d alumnos\n\n", Carreras[max2], cantmax2[max2]);
             break;
         case 4:
-            flag = 0;
+            Menu = 0;        // Se deja de visualizar el menu
+            int max3 = 0;        // Variable a la que se le asignara el numero de los alumnos
+            int cantmax3[5] = {0}; // Creacion de un arreglo unidimensional para los anios
+
+            // Doble ciclo For que coloca al total de alumnos que fueron matriculados en la carrera de Ingenieria de Software en cada anio
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    cantmax3[i] = estudiantesmat[i][0][j];
+                }
+            }
+
+            // Uso de un ciclo For y un condicional If para poder encontrar el anio con mas cantidad de alumnos en la carrera de Ingenieria de Software en el segundo semestre
+            max3 = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (cantmax3[i] > cantmax3[max3])
+                {
+                    max3 = i;
+                }
+            }
+
+            // Imprime la respuesta
+            printf("Respuesta: El anio en que la carrera de Ingenieria de Software recibio la mayor cantidad de alumnos en el segundo semestre fue el %d con %d alumnos\n\n", max3 + 1, cantmax3[max3]);
+            break;
+        case 5:
+            Menu = 0; // Se deja de visualizar el menu
+            flag = 0; // La variable booleana se vuelve falsas y esto hace que se cierre el programa
             break;
         default:
             printf("\nRespuesta invalida. Ingrese de nuevo\n");
             break;
         }
-
+        // Se coloca un texto que permite que el usuario despues de dar entre, vuelva al menu
         getchar();
         printf("Presione ENTER para regresar al menu...");
         getchar();
-        system("clear");
-    } while (flag);
-
+    }
+    // Esto permite que mientras la variable siga siendo verdadera, se reinicie el menu
     return flag;
-}
-
-int main(){
-
-    srand(time(NULL));
-
-    int flag = 1;
-    int alumnosMatri[NUM_ANIOS][NUM_CARRERAS][NUM_GENEROS];
-
-    for (int i = 0; i < NUM_ANIOS; i++) {
-        for (int j = 0; j < NUM_CARRERAS; j++) {
-            for (int k = 0; k < NUM_GENEROS; k++) {
-                alumnosMatri[i][j][k] = rand()%101;
-            }
-        }
-    }
-
-    while (flag) {
-        flag = menumostrar(flag, alumnosMatri);
-    }
-
-    return 0;
 }
